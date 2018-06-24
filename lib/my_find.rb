e01f2c4d938c7b62daee78f0bc1b47e2711c4382
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-24 02:04:41 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,7 @@
 def my_find(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
-      return collection[i] if yield(collection[i])
-    end
+    return collection[i] if yield(collection[i])
     i = i + 1
   end
 end
